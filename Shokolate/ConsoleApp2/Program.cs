1a2777678f94d46dd13e50b45419593d09a9c3df
Сокол <118041104+Sokol-33@users.noreply.github.com>
2023-11-23 09:26:42 +0300

    Add files via upload

---
+++ Shokolate/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int money = 15;   // Имеющиеся деньги
+        int price = 1;    // Цена за шоколадку
+        int wrap = 3;     // Количество оберток для получения дополнительной шоколадки
+
+        int chocolates = money / price;  // Количество шоколадок, которое можно купить за имеющиеся деньги
+        int wrappers = chocolates;       // Изначально количество оберток равно количеству купленных шоколадок
+
+        while (wrappers >= wrap)
+        {
+            int extraChocolates = wrappers / wrap;  // Количество дополнительных шоколадок, которые можно получить за обертки
+            chocolates += extraChocolates;          // Увеличиваем общее количество шоколадок
+            wrappers = wrappers % wrap + extraChocolates;  // Обновляем количество оставшихся оберток
+        }
+
+        Console.WriteLine($"Максимальное количество шоколадок: {chocolates}");
+    }
+}
